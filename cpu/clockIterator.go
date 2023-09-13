825544af7d731ff3e9681656b127a3a9de5f10cb
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2023-09-13 20:46:14 +0200

    clock iterator i bättre läge

--- cpu/clockIterator.go
+++ cpu/clockIterator.go
@@ -21,7 +21,7 @@ func InitClock(input pars.InputIter) ClockIterator {
 	return ClockIterator{inputIter: &input, delayedAdd: delayedAdd, registerValue: registerValue}
 }
 
-func InitClockTest(input []pars.Input) ClockIterator{
+func InitClockTest(input []pars.Input) ClockIterator {
 	var inputIter pars.TestIter = pars.InitTestInput(input)
 	delayedAdd := DelayedAdd{valueToAdd: 0, delay: 0}
 	return ClockIterator{inputIter: &inputIter, delayedAdd: delayedAdd, registerValue: 1}
@@ -29,17 +29,26 @@ func InitClockTest(input []pars.Input) ClockIterator{
 
 func (c ClockIterator) HasNext() bool {
 	haveInput := c.inputIter.HasNext()
-	computesInput := c.delayedAdd.delay > 0
+	computesInput := c.delayedAdd.delay >= 0
 	return haveInput || computesInput
 }
 
 func (c *ClockIterator) GetNext() int {
-	retValue := c.registerValue
-	if c.delayedAdd.delay == 0 {
-		c.registerValue += c.delayedAdd.valueToAdd //skicka runt operatorer med delay istället ??
+	c.registerValue += c.delayedAdd.valueToAdd //skicka runt operatorer med delay istället ??
+
+	if c.inputIter.HasNext() {
 		input := c.inputIter.GetNext()
 		c.delayedAdd = DelayedAdd{valueToAdd: input.Value, delay: input.ExecutionTime}
 	}
+	return c.registerValue
+}
+
+func (c *ClockIterator) GetNextCycle() int {
+	currentValue := c.registerValue
+
+	if c.delayedAdd.delay < 1 {
+		currentValue = c.GetNext()
+	}
 	c.delayedAdd.delay -= 1
-	return retValue
+	return currentValue
 }
--- cpu/clockIterator_test.go
+++ cpu/clockIterator_test.go
@@ -5,20 +5,25 @@ import (
 	"testing"
 )
 
-
+/*
 func TestShortest(t *testing.T) {
 	input := pars.InitInputIter("../input_short.txt")
 	cpu := InitClock(input)
 	for cpu.HasNext() {
-		println(cpu.GetNext())
-	}
-}
-
-/*
-func TestArray(t *testing.T) {
-	inputs := []pars.Input{{ExecutionTime: 1, Value: 0}, {ExecutionTime: 2, Value: 3}, {ExecutionTime: 2, Value: -5}}
-	cpu := InitClockTest(inputs)
-	for cpu.HasNext() {
-		println(cpu.GetNext())
+		println(cpu.GetNextCycle())
 	}
 }*/
+
+func TestArray(t *testing.T) {
+	inputs := []pars.Input{{ExecutionTime: 1, Value: 0},
+		{ExecutionTime: 2, Value: 3},
+		{ExecutionTime: 2, Value: -5}}
+	cpu := InitClockTest(inputs)
+	lastValue := 0
+	for cpu.HasNext() {
+		lastValue = cpu.GetNextCycle()
+	}
+	if lastValue != -1 {
+		t.Errorf("Wrong value at end of short program , is %d , should be %d", lastValue, -1)
+	}
+}
