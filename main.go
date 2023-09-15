d3bc63bc411d0a30d713e9510f5b9d87b652eab7
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2023-09-15 19:20:19 +0200

    part 2

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	println(part1())
+	part2()
 }
 
 func part1() int {
 	inputIter := pars.InitInputIter("input.txt")
 	cpu := cpu.InitClock(inputIter)
-	//Vilka värden var intressanta
 	importantCycles := []int{20, 60, 100, 140, 180, 220}
 	importantCyclesIndex := 0
 	currentCycle := 1
@@ -25,8 +25,31 @@ func part1() int {
 			sum += registerValue * currentCycle
 			fmt.Printf("cycle %d , registervalue %d , sum %d  \n", currentCycle, registerValue, sum)
 		}
-		//println(registerValue)
+		println(registerValue)
 		currentCycle++
 	}
 	return sum
+}
+
+func part2() {
+	inputIter := pars.InitInputIter("input.txt")
+	cpu := cpu.InitClock(inputIter)
+	currentPixel := 1
+	for cpu.HasNext() {
+		spritePositon := cpu.GetNextCycle()
+		if pixelInSprite(spritePositon, currentPixel) {
+			fmt.Printf("#")
+		} else {
+			fmt.Printf(".")
+		}
+		if currentPixel%40 == 0 {
+			fmt.Printf("\n")
+			currentPixel = 0
+		}
+		currentPixel++
+	}
+}
+
+func pixelInSprite(spritePositon int, currentPixel int) bool {
+	return spritePositon <= currentPixel && currentPixel < spritePositon+3
 }
